d63c0810e38b6c25278b645488d69d512fbe2567
Jonathan Treffler <jonathan.treffler@verdigado.com>
2024-10-15 17:11:40 +0200

    added InvalidResourceType error

---
+++ lib/Errors/InvalidResourceType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OCA\OrganizationFolders\Errors;
+
+class InvalidResourceType extends \RuntimeException {
+	public function __construct(string $type) {
+        parent::__construct($type . " is not a valid resource type");
+	}
+}
