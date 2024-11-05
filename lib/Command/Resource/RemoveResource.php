4dad10d23f221e55e55a52c7f78cbea7496cb53e
Jonathan Treffler <jonathan.treffler@verdigado.com>
2024-11-05 13:46:06 +0100

    added occ command to remove resource

---
+++ lib/Command/Resource/RemoveResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace OCA\OrganizationFolders\Command\Resource;
+
+use OCP\DB\Exception;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use OCA\OrganizationFolders\Command\BaseCommand;
+
+class RemoveResource extends BaseCommand {
+	protected function configure(): void {
+		$this
+			->setName('organization-folders:remove-resource')
+			->setDescription('Remove a resource')
+			->addArgument('id', InputArgument::REQUIRED, 'Id of the resource');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$id = (int)$input->getArgument('id');
+
+		try {
+			$this->resourceService->deleteById($id);
+
+            $output->writeln("done");
+
+			return 0;
+		} catch (Exception $e) {
+			$output->writeln("<error>Exception \"{$e->getMessage()}\" at {$e->getFile()} line {$e->getLine()}</error>");
+			return 1;
+		}
+	}
+}
