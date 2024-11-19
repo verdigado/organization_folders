01d63a7f76d7348e547c3e336da329504b56bdd3
Jonathan Treffler <jonathan.treffler@verdigado.com>
2024-11-19 19:56:15 +0100

    Added organization controller

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -8,5 +8,10 @@ return [
 		['name' => 'resource#show', 'url' => '/resources/{resourceId}', 'verb' => 'GET'],
 		['name' => 'resource#create', 'url' => '/resources/{resourceId}', 'verb' => 'POST'],
 		['name' => 'resource#update', 'url' => '/resources/{resourceId}', 'verb' => 'PUT'],
+		['name' => 'organization#getOrganizationProviders', 'url' => '/organizationProviders', 'verb' => 'GET'],
+		['name' => 'organization#getOrganization', 'url' => '/organizationProviders/{organizationProviderId}/organizations/{organizationId}', 'verb' => 'GET'],
+		['name' => 'organization#getSubOrganizations', 'url' => '/organizationProviders/{organizationProviderId}/organizations/{parentOrganizationId}/subOrganizations', 'verb' => 'GET'],
+		['name' => 'organization#getTopLevelOrganizations', 'url' => '/organizationProviders/{organizationProviderId}/subOrganizations', 'verb' => 'GET'],
+		['name' => 'organization#getRoles', 'url' => '/organizationProviders/{organizationProviderId}/organizations/{organizationId}/roles/', 'verb' => 'GET'],
 	],
 ];
---
+++ lib/Controller/OrganizationController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OCA\OrganizationFolders\Controller;
+
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+
+use OCA\OrganizationFolders\OrganizationProvider\OrganizationProviderManager;
+
+class OrganizationController extends BaseController {
+	use Errors;
+
+	public function __construct(
+		private OrganizationProviderManager $oganizationProviderManager,
+    ) {
+		parent::__construct();
+	}
+
+    #[NoAdminRequired]
+	public function getOrganizationProviders(): JSONResponse {
+		return $this->handleErrors(function () {
+			return array_keys($this->oganizationProviderManager->getOrganizationProviders());
+		});
+	}
+
+	#[NoAdminRequired]
+    public function getOrganization(string $organizationProviderId, int $organizationId): JSONResponse {
+		return $this->handleErrors(function () use ($organizationProviderId, $organizationId) {
+			$organizationProvider = $this->oganizationProviderManager->getOrganizationProvider($organizationProviderId);
+
+			return $organizationProvider->getOrganization($organizationId);
+		});
+    }
+
+	#[NoAdminRequired]
+	public function getTopLevelOrganizations(string $organizationProviderId): JSONResponse {
+		return $this->handleErrors(function () use ($organizationProviderId) {
+			$organizationProvider = $this->oganizationProviderManager->getOrganizationProvider($organizationProviderId);
+
+			return $organizationProvider->getSubOrganizations();
+		});
+    }
+
+	#[NoAdminRequired]
+    public function getSubOrganizations(string $organizationProviderId, int $parentOrganizationId): JSONResponse {
+		return $this->handleErrors(function () use ($organizationProviderId, $parentOrganizationId) {
+			$organizationProvider = $this->oganizationProviderManager->getOrganizationProvider($organizationProviderId);
+
+			return $organizationProvider->getSubOrganizations($parentOrganizationId);
+		});
+    }
+
+	#[NoAdminRequired]
+	public function getRoles(string $organizationProviderId, int $organizationId): JSONResponse {
+		return $this->handleErrors(function () use ($organizationProviderId, $organizationId) {
+			$organizationProvider = $this->oganizationProviderManager->getOrganizationProvider($organizationProviderId);
+
+			return $organizationProvider->getRolesOfOrganization($organizationId);
+		});
+	}
+}
