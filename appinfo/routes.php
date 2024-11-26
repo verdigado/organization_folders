d9bea13b44ec874e2fc2b3c8172f5dc2f3cdeffd
Jonathan Treffler <jonathan.treffler@verdigado.com>
2024-11-26 23:57:28 +0100

    fixed resource create route

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -6,7 +6,7 @@ return [
 	'routes' => [
 		/* Resources */
 		['name' => 'resource#show', 'url' => '/resources/{resourceId}', 'verb' => 'GET'],
-		['name' => 'resource#create', 'url' => '/resources/{resourceId}', 'verb' => 'POST'],
+		['name' => 'resource#create', 'url' => '/resources', 'verb' => 'POST'],
 		['name' => 'resource#update', 'url' => '/resources/{resourceId}', 'verb' => 'PUT'],
 		['name' => 'resource#subResources', 'url' => '/resources/{resourceId}/subResources', 'verb' => 'GET'],
 		['name' => 'resource_member#index', 'url' => '/resources/{resourceId}/members', 'verb' => 'GET'],
