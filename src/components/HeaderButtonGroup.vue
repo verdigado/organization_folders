b69d4d9ec76eeede2b4e8f923b1acbe3ac5c40b8
Jonathan Treffler <jonathan.treffler@verdigado.com>
2025-02-19 14:07:43 +0100

    added header button combination group component

---
+++ src/components/HeaderButtonGroup.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header-button-group">
+        <slot />
+    </div>
+</template>
+
+<style>
+.header-button-group {
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	column-gap: 10px;
+	margin-top: 24px;
+	margin-bottom: 12px;
+
+	h1, h2, h3 {
+		margin-top: 0px;
+	}
+}
+</style>
