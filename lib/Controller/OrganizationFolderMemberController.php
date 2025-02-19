d059553bf5e2092080eac96f36a343c09745b2cd
Jonathan Treffler <jonathan.treffler@verdigado.com>
2025-02-19 01:08:10 +0100

    added organization folder member controller

---
+++ lib/Controller/OrganizationFolderMemberController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace OCA\OrganizationFolders\Controller;
+
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+
+use OCA\OrganizationFolders\Db\OrganizationFolderMember;
+use OCA\OrganizationFolders\Service\OrganizationFolderService;
+use OCA\OrganizationFolders\Service\OrganizationFolderMemberService;
+use OCA\OrganizationFolders\Enum\PrincipalType;
+use OCA\OrganizationFolders\Enum\OrganizationFolderMemberPermissionLevel;
+use OCA\OrganizationFolders\Model\PrincipalFactory;
+
+class OrganizationFolderMemberController extends BaseController {
+	use Errors;
+
+	public function __construct(
+		private OrganizationFolderMemberService $service,
+		private OrganizationFolderService $organizationFolderService,
+		private PrincipalFactory $principalFactory,
+	) {
+		parent::__construct();
+	}
+
+	#[NoAdminRequired]
+	public function index(int $organizationFolderId): JSONResponse {
+		return $this->handleNotFound(function () use ($organizationFolderId) {
+			$organizationFolder = $this->organizationFolderService->find($organizationFolderId);
+
+			$this->denyAccessUnlessGranted(['READ'], $organizationFolder);
+
+			return $this->service->findAll($organizationFolderId);
+		});
+	}
+
+	#[NoAdminRequired]
+	public function create(
+		int $organizationFolderId,
+		string|int $permissionLevel,
+		string|int $principalType,
+		string $principalId,
+	): JSONResponse {
+		return $this->handleErrors(function () use ($organizationFolderId, $permissionLevel, $principalType, $principalId): OrganizationFolderMember {
+			$organizationFolder = $this->organizationFolderService->find($organizationFolderId);
+
+			$this->denyAccessUnlessGranted(['UPDATE_MEMBERS'], $organizationFolder);
+
+			$principal = $this->principalFactory->buildPrincipal(PrincipalType::fromNameOrValue($principalType), $principalId);
+
+			$organizationFolderMember = $this->service->create(
+				organizationFolderId: $organizationFolderId,
+				permissionLevel: OrganizationFolderMemberPermissionLevel::fromNameOrValue($permissionLevel),
+				principal: $principal,
+			);
+
+			return $organizationFolderMember;
+		});
+	}
+
+	#[NoAdminRequired]
+	public function update(
+		int $id,
+		string|int $permissionLevel,
+	): JSONResponse {
+		return $this->handleErrors(function () use ($id, $permissionLevel): OrganizationFolderMember {
+			$organizationFolderMember = $this->service->find($id);
+
+			$organizationFolder = $this->organizationFolderService->find($organizationFolderMember->getOrganizationFolderId());
+			
+			$this->denyAccessUnlessGranted(['UPDATE_MEMBERS'], $organizationFolder);
+
+			$organizationFolderMember = $this->service->update(
+				id: $organizationFolderMember->getId(),
+				permissionLevel: OrganizationFolderMemberPermissionLevel::fromNameOrValue($permissionLevel),
+			);
+
+			return $organizationFolderMember;
+		});
+	}
+
+	#[NoAdminRequired]
+	public function destroy(int $id): JSONResponse {
+		return $this->handleNotFound(function () use ($id): OrganizationFolderMember {
+			$organizationFolderMember = $this->service->find($id);
+
+			$organizationFolder = $this->organizationFolderService->find($organizationFolderMember->getOrganizationFolderId());
+			
+			$this->denyAccessUnlessGranted(['UPDATE_MEMBERS'], $organizationFolder);
+
+			return $this->service->delete($organizationFolderMember->getId());
+		});
+	}
+}
