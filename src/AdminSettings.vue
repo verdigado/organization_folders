ccfa4b46cecbd120fefb63b5f26ceac7e1445f7a
Jonathan Treffler <jonathan.treffler@verdigado.com>
2025-10-21 13:31:41 +0200

    fixed adminSettings formatting

--- src/AdminSettings.vue
+++ src/AdminSettings.vue
@@ -16,48 +16,46 @@
 		</div>
 	  </NcSettingsSection>
 	</div>
-  </template>
+</template>
   
-  <script setup>
-  import { ref } from "vue";
-  import debounceFunction from "debounce-fn";
-  import { translate as t, translatePlural as n } from "@nextcloud/l10n";
+<script setup>
+import { ref } from "vue";
+import debounceFunction from "debounce-fn";
+import { translate as t, translatePlural as n } from "@nextcloud/l10n";
 
-  import NcSettingsSection from "@nextcloud/vue/components/NcSettingsSection";
-  import NcTextField from "@nextcloud/vue/components/NcTextField";
-  import NcPasswordField from "@nextcloud/vue/components/NcPasswordField";
-  import NcCheckboxRadioSwitch from "@nextcloud/vue/components/NcCheckboxRadioSwitch";
+import NcSettingsSection from "@nextcloud/vue/components/NcSettingsSection";
+import NcTextField from "@nextcloud/vue/components/NcTextField";
+import NcPasswordField from "@nextcloud/vue/components/NcPasswordField";
+import NcCheckboxRadioSwitch from "@nextcloud/vue/components/NcCheckboxRadioSwitch";
 
+import { adminSettingsApi } from "./adminSettingsApi.js";
 
-  import { adminSettingsApi } from "./adminSettingsApi.js";
-  
-  let loading = ref(true);
-  let settings = ref({});
-  
-  const app_settings = [
+let loading = ref(true);
+let settings = ref({});
+
+const app_settings = [
 	{id: "subresources_enabled", name: t("organization_folders", "Subresources Feature Enabled"), bool: true},
-	{id: "hide_virtual_groups", name: t("organization_folders", "Hide Virtual Groups"), bool: true}
-  ];
-  
-  adminSettingsApi.getAllSettings().then((result) => {
+	{id: "hide_virtual_groups", name: t("organization_folders", "Hide Virtual Groups"), bool: true},
+];
+
+adminSettingsApi.getAllSettings().then((result) => {
 	settings.value = result;
 	loading.value = false;
-  });
-  
-  const updateSetting = debounceFunction((key, value) => {
+});
+
+const updateSetting = debounceFunction((key, value) => {
 	console.log("updateSetting", key, value);
 	settings.value[key] = value;
-	adminSettingsApi.setSetting(key, value).then(() => {
-  
-	});
-  }, {
+	adminSettingsApi.setSetting(key, value).then(() => {});
+},
+{
 	wait: 1000,
-  })
-  </script>
+});
+</script>
   
-  <style scoped>
-  .settings_field {
+<style scoped>
+.settings_field {
 	margin-bottom: 20px;
-  }
-  </style>
+}
+</style>
   
